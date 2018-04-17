bd41f588d84669c705b8cac6c6f9743c26ae650f
ojcarrillo <oj_carrillo@yahoo.com>
2018-04-17 21:24:44 +0000

    ajuste puerto ftp

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ mvn clean package -Dmaven.test.skip=true
 echo "======================================================="
 echo "======================================================="
 echo "compila imagen docker - dk_receivermq_cont "
-docker build -t testmq .
+docker build -t dk_receivermq_cont .
 echo "======================================================="
 echo "======================================================="
 echo "corre imagen del contenedor"
 docker run --name dk_receivermq_cont --net=backend \
--d -p 2021:21 -p 2020:20 -p 12020:12020 -p 12021:12021 -p 12022:12022 -p 12023:12023 -p 12024:12024 -p 12025:12025  \
+-d -p 4021:21 -p 4020:20 -p 14020:12020 -p 14021:12021 -p 14022:12022 -p 14023:12023 -p 14024:12024 -p 14025:12025  \
 -e "USER=touresbalon" -e "PASS=verysecretpwd" \
 -v /data/ftp/receivermq_cont:/ftp \
 -it dk_receivermq_cont
